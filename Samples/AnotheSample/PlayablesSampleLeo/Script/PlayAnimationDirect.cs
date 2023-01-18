649e0215c813a7a02f410b92cbd7b5b3178c1658
Leonardo Quinones <leonardo.quinones@unity3d.com>
2023-01-18 16:16:56 -0500

    Test with a managed plugin

--- Samples/AnotheSample/PlayablesSampleLeo/Script/PlayAnimationDirect.cs
+++ Samples/AnotheSample/PlayablesSampleLeo/Script/PlayAnimationDirect.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using DLLTest;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Playables;
@@ -11,9 +12,22 @@ public class PlayAnimationDirect : MonoBehaviour
     public AnimationClip mclip;
     PlayableGraph _playableGraph;
 
+    public Transform prefab_ref;
+
     private void Start()
     {
         AnimationPlayableUtilities.PlayClip(GetComponent<Animator>(), mclip, out _playableGraph);
+        //Debug.Log(addval (5, 20));
+        var h = new MyUtilities();
+        h.AddValues(4,3);
+        Debug.Log(h.c);
+        
+        InstatiateObject();
+    }
+
+    public void InstatiateObject()
+    {
+        Instantiate(prefab_ref, new Vector3(0.0f, 0, 0), Quaternion.identity);
     }
 
     private void OnDisable()
