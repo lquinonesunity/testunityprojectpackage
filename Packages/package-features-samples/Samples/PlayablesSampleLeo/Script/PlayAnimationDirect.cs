96b21ea9ce586b5c491e1ac15e5c48ccd0c6149e
Leonardo Quinones <leonardo.quinones@unity3d.com>
2023-01-23 00:38:04 -0500

    Upload playables practice

--- Packages/package-features-samples/Samples/PlayablesSampleLeo/Script/PlayAnimationDirect.cs
+++ Packages/package-features-samples/Samples/PlayablesSampleLeo/Script/PlayAnimationDirect.cs
@@ -24,7 +24,7 @@ public class PlayAnimationDirect : MonoBehaviour
         
         h.CallToShowText();
         
-        InstatiateObject();
+        //InstatiateObject();
         //ShowText();
     }
 
