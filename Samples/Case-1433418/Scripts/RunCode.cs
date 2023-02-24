2f3e9f419d1e4e81195b3470067e5fb87523e0f2
Leonardo Quinones <leonardo.quinones@unity3d.com>
2023-02-24 16:04:28 -0500

    Updating DLL Library

---
+++ Samples/Case-1433418/Scripts/RunCode.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DLLTest;
+
+public class RunCode : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        var myInstance = new MyUtilities();
+        
+        //Class 1
+        myInstance.MyMethod_One();
+        
+        //Class 2
+        myInstance.MyMethod_Two();
+        
+        // new method
+        myInstance.MyMethod_Three();
+    }
+}
