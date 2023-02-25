f42bd403a70b5c1d8ad1836aacb1699206947076
Leonardo Quinones <leonardo.quinones@unity3d.com>
2023-02-25 13:20:50 -0500

    Updating DLL Library

--- Samples/Case-1433418/Scripts/RunCode.cs
+++ Samples/Case-1433418/Scripts/RunCode.cs
@@ -11,33 +11,7 @@ public class RunCode : MonoBehaviour
         var myInstance = new MyUtilities();
         
         //Class 1
-        myInstance.MyMethod_One();
-        
-        //Class 2
-        myInstance.MyMethod_Two();
-        
-        // new method
-        myInstance.MyMethod_Three();
-        
-        // justo a try
-        Debug.Log("Just a try");
-        
-        // 4th method
-        myInstance.MyMethodFour();
-        
-        // 5th method
-        myInstance.MyMethod_Five();
-        
-        // 6th method
-        myInstance.MyMethod_S();
-        
-        // 7th method
-        myInstance.MyMethod_Seven();
-        
-        // 8th method
-        myInstance.MyMethod_Eight();
-        
-        // 9th method
-        myInstance.MyMethod_Nine();
+        myInstance.TooManyMethods();
+        Debug.Log("Just one method");
     }
 }
