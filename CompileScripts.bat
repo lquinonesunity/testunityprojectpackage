cd300c0680712ee1b50fb9d9ff332b59935d8c62
Leonardo Quinones <leonardo.quinones@unity3d.com>
2023-02-25 12:49:08 -0500

    Updating DLL Library

--- CompileScripts.bat
+++ CompileScripts.bat
@@ -1,4 +1,4 @@
-C:\Program Files\Microsoft Visual Studio\2022\Professional\VC\Auxiliary\Build\vcvars64.bat
+
 cd Assets\Framework\
 copy *.cs D:\lquinones\SampleCSharpPlugin\DLLTest_GetFilesFromUnity
 cd D:\lquinones\SampleCSharpPlugin\DLLTest_GetFilesFromUnity
