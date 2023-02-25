6bc98747eb0c5cb9e9bb0faac6f00d84c313c339
Leonardo Quinones <leonardo.quinones@unity3d.com>
2023-02-25 13:20:50 -0500

    Updating DLL Library

--- Assets/Framework/MyUtilities.cs
+++ Assets/Framework/MyUtilities.cs
@@ -4,55 +4,10 @@ using UnityEngine;
 
 namespace DLLTest {
     public class MyUtilities {
-        public void MyMethod_One()
+        public void TooManyMethods()
         {
             Debug.Log("MyMethod_One(): ");
             Class1.ShowTextFromResources();
-        }
-        public void MyMethod_Two()
-        {
-            Debug.Log("MyMethod_Two(): ");
-            Class2.ShowAMessage();
-        }
-
-        public void MyMethod_Three()
-        {
-            Debug.Log("MyM 3: ");
-            Class1.ShowTextFromResources();
-            Class2.ShowAMessage();
-            Debug.Log("New Message at 11.51");
-        }
-
-        public void MyMethodFour()
-        {
-            Debug.Log("Look at: ");
-            Debug.Log("My Log");
-            Class2.ShowAMessage();
-        }
-
-        public void MyMethod_Five()
-        {
-            Debug.Log("Five ");
-        }
-
-        public void MyMethod_S()
-        {
-            Debug.Log("S ");
-        }
-
-        public void MyMethod_Seven()
-        {
-            Debug.Log("Seven ");
-        }
-
-        public void MyMethod_Eight()
-        {
-            Debug.Log("Eight 8");
-        }
-
-        public void MyMethod_Nine()
-        {
-            Debug.Log("Nine");
         }
     }
 }
