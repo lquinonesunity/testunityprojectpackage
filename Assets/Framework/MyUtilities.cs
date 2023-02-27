8e263748c874e881ae77c6e420b3a94f14d8e8ed
Leonardo Quinones <leonardo.quinones@unity3d.com>
2023-02-27 09:57:56 -0500

    Updating DLL Library

--- Assets/Framework/MyUtilities.cs
+++ Assets/Framework/MyUtilities.cs
@@ -9,5 +9,11 @@ namespace DLLTest {
             Debug.Log("MyMethod_One(): ");
             Class1.ShowTextFromResources();
         }
+        public void TooManyMethods_2()
+        {
+            Debug.Log("MyMethod_One(): ");
+            Class1.ShowTextFromResources();
+            Class2.ShowAMessage();
+        }
     }
 }
